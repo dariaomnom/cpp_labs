ece5c8267ef8810756d386d1f26f6fc915b0d846
dariaomnom <dariaomnom@gmail.com>
2023-10-27 12:12:50 +0300

    12:12

--- GameFieldCreator.cpp
+++ GameFieldCreator.cpp
@@ -14,29 +14,31 @@ void GameFieldCreator::createField(int level) {
         passable = 5;
         points = 3;
         position = 6;
-//        enemies = 5;
-        enemies = field.getSize().first * field.getSize().first / 10 - 1;
-        if (enemies == points || enemies == position) enemies += 1;
-        lives = enemies * 3;
-        if (lives == points || lives == position) lives += 1;
+        enemies = 5;
+//        enemies = field.getSize().first * field.getSize().second / 10 - 1;
+//        if (enemies == points || enemies == position) enemies += 1;
+        lives = enemies * 2;
+//        if (lives == points || lives == position) lives += 1;
     } else if (level == 2) {
         passable = 3;
         points = 2;
         position = 5;
-//        enemies = 3;
-        enemies = field.getSize().first * field.getSize().first / 15 - 1;
-        if (enemies == points || enemies == position) enemies -= 1;
-        lives = enemies * 3;
-        if (lives == points || lives == position) lives += 1;
+        enemies = 3;
+//        enemies = field.getSize().first * field.getSize().second / 15 - 1;
+//        if (enemies == points || enemies == position)
+//            enemies -= 1;
+        lives = enemies * 2;
+//        if (lives == points || lives == position) lives += 1;
     } else {
         passable = 4;
         points = 3;
         position = 5;
-//        enemies = 3;
-        enemies = field.getSize().first * field.getSize().first / 10 - 2;
-        if (enemies == points || enemies == position) enemies -= 1;
+        enemies = 4;
+//        enemies = field.getSize().first * field.getSize().first / 10 - 2;
+//        if (enemies == points || enemies == position)
+//            enemies -= 1;
         lives = enemies * 3;
-        if (lives == points || lives == position) lives += 1;
+//        if (lives == points || lives == position) lives += 1;
     }
 
     int x = 0; int y = 0;
@@ -46,16 +48,29 @@ void GameFieldCreator::createField(int level) {
         if (x != field.getEntry().first && y != field.getEntry().second && x != field.getExit().first && y != field.getExit().second)
             field.getCell(x,y).setPassable(false);
     }
+
     for (int j = 0; j < field.getSize().second; j++) {
         for (int i = 0; i < field.getSize().first; i++) {
             if (!field.getCell(i,j).getEvent() && field.getCell(i,j).isPassable()) {
-                if (((j+1) * field.getSize().first + i + 1) % points == 0) {
+
+//                if (((j+1) * field.getSize().first + i + 1) % points == 0)
+                if ((j-i) % points == 0)
+                {
                     field.getCell(i,j).setEvent(new PointsEvent(player));
-                } else if (((j+1) * field.getSize().first + i + 1) % enemies == 0) {
+                }
+                else if ((j-i) % enemies == 0)
+//                    if (((j+1) * field.getSize().first + i + 1) % enemies == 0)
+                {
                     field.getCell(i,j).setEvent(new EnemyEvent(player));
-                } else if (((j+1) * field.getSize().first + i + 1) % position == 0) {
+                }
+                else if ((j-i) % position == 0)
+//                    if (((j+1) * field.getSize().first + i + 1) % position == 0)
+                {
                     field.getCell(i,j).setEvent(new PositionEvent(player, controller, field));
-                } else if (((j+1) * field.getSize().first + i + 1) % lives == 0) {
+                }
+                else if ((j-i) % lives == 0)
+//                    if (((j+1) * field.getSize().first + i + 1) % lives == 0)
+                {
                     field.getCell(i,j).setEvent(new LivesEvent(player));
                 }
             }
